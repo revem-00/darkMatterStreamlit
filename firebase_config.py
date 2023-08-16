ceb9be998b400fa24803e22f545445e96b09b768
Eduardo-Vega <129542392+revem-00@users.noreply.github.com>
2023-08-16 13:55:04 -0600

    dep

---
+++ firebase_config.py
@@ -0,0 +1,8 @@
+
+config = {'apiKey': "AIzaSyDzsEUJjT8dVsYqgNWXaVfV7KX7Hfgr3tc",
+    'authDomain': "darkmatter-b5575.firebaseapp.com",
+    'projectId': "darkmatter-b5575",
+    'databaseURL': "https://darkmatter-b5575-default-rtdb.firebaseio.com",
+    'storageBucket': "darkmatter-b5575.appspot.com",
+    'messagingSenderId': "957172654257",
+    'appId': "1:957172654257:web:4d4fb436ab72ddc0d8c290"}
